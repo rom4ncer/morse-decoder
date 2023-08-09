701a19091e5a8385b879ab639506e70b6548c2e0
Roman Tarasov <rom4ncer@gmail.com>
2023-08-09 23:45:41 +0300

    init: answer

--- src/index.js
+++ src/index.js
@@ -37,8 +37,30 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+const DECODE_TABLE = {
+    '00': '',
+    '10': '.',
+    '11': '-',
+    '**': '**'
+};
+
 function decode(expr) {
     // write your solution here
+    answer = [];
+    arr = [];
+    while (expr.length > 0) {
+        arr.push(expr.slice(0, 10));
+        expr = expr.slice(10);
+    } 
+    arr.forEach( (item) => {
+        let word = '';
+        for (let i = 0; i < 10; i += 2) {
+            let char = item.slice(i, i + 2);
+            word += DECODE_TABLE[char];
+        }
+        (word == '**********') ? answer += [' '] : answer += [MORSE_TABLE[word]];
+    });
+    return answer;
 }
 
 module.exports = {
